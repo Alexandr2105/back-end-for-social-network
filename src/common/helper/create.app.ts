b477aa349c0960253e7ba6dae254143fbaa82a46
Alexandr2105 <5030553@gmail.com>
2024-01-10 13:14:15 +0300

    return all back

--- src/common/helper/create.app.ts
+++ src/common/helper/create.app.ts
@@ -6,45 +6,39 @@ import {
 import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from '../../exception.filter';
 
-export let createApp: any;
-
-try {
-  createApp = (app: INestApplication) => {
-    app.use(cookieParser());
-    app.enableCors({
-      origin: [
-        'http://localhost:63342',
-        'http://localhost:3000',
-        'https://social-network-front-old-version.vercel.app',
-      ],
-      methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-      credentials: true,
-      allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
-    });
-    app.useGlobalPipes(
-      new ValidationPipe({
-        forbidUnknownValues: false,
-        stopAtFirstError: true,
-        exceptionFactory: (errors) => {
-          const errorsForResponse = [];
-          errors.forEach((e) => {
-            const constraintsKeys = Object.keys(e.constraints);
-            constraintsKeys.forEach((c) => {
-              errorsForResponse.push({
-                message: e.constraints[c],
-                field: e.property,
-              });
+export const createApp = (app: INestApplication) => {
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [
+      'http://localhost:63342',
+      'http://localhost:3000',
+      'https://social-network-front-old-version.vercel.app',
+    ],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: false,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+        errors.forEach((e) => {
+          const constraintsKeys = Object.keys(e.constraints);
+          constraintsKeys.forEach((c) => {
+            errorsForResponse.push({
+              message: e.constraints[c],
+              field: e.property,
             });
           });
-          throw new BadRequestException(errorsForResponse);
-        },
-      }),
-    );
-    app.useGlobalFilters(new HttpExceptionFilter());
-    return app;
-  };
-} catch (e) {
-  console.log(e);
-}
+        });
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  return app;
+};
