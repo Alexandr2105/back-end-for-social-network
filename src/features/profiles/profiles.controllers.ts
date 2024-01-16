8483b83fea26b44ab998743cea805679427ccb54
Alexandr2105 <5030553@gmail.com>
2024-01-16 11:52:03 +0300

    change profiles.controllers.ts method createOrUpdateProfile del userId in url

--- src/features/profiles/profiles.controllers.ts
+++ src/features/profiles/profiles.controllers.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateOrUpdateProfileCommand } from './application/useCases/create.or.update.profile.use-case';
 import { ProfileDto } from './dto/profile.dto';
@@ -10,6 +18,7 @@ import {
   SwaggerDecoratorByGetProfile,
 } from './swagger/swagger.profile.decorators';
 import { GetProfileForCurrentUserCommand } from './application/useCases/get.profile.for.current.user.use-case';
+import { RefreshAuthGuard } from '../../common/guards/refresh.auth.guard';
 
 @ApiTags('Profile')
 @Controller('profile')
@@ -17,13 +26,14 @@ export class ProfilesControllers {
   constructor(private readonly commandCommandBus: CommandBus) {}
 
   @SwaggerDecoratorByCreateOrUpdateProfile()
-  @Post(':userId')
+  @UseGuards(RefreshAuthGuard)
+  @Post()
   async createOrUpdateProfile(
     @Body() body: ProfileDto,
-    @Param() param: UserIdDto,
+    @Req() req: any,
   ): Promise<ProfileEntity> {
     return this.commandCommandBus.execute(
-      new CreateOrUpdateProfileCommand(param, body),
+      new CreateOrUpdateProfileCommand(req.user.userId, body),
     );
   }
 
